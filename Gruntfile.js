0835c8a70de3d52dc3b2815bd39803ea16af8e75
Vitaliy <vitaliy@altsolution.net>
2017-06-14 20:08:58 +0300

    fix menu btn

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   // подключаем плагин load-grunt-tasks, чтобы не перечислять все прочие плагины
   require('load-grunt-tasks')(grunt);
 
-  // описываем задачи, которые планируем использовать
+  // описываем задачи, которые планируем использовать (их запуск - см. низ этого файла)
   grunt.initConfig({
 
     // очищаем папку build перед новой записью
@@ -11,25 +11,81 @@ module.exports = function(grunt) {
       build: ['build']
     },
 	
+	// компилируем scss
+    sass: {
+      source: {
+        files: [{
+          expand: true,
+          cwd: 'source/scss',
+          src: ['*.scss'],
+          dest: 'source/css',
+          ext: '.css'
+        }]
+      }
+    },
+	
+
 	// копируем файлы из папки source в папку build и дальше работаем с этой папкой
     copy: {
-      build: {
-		files: [{
-			expand: true,
-			cwd: "source",
-			src: [
-			'img/**',             
-            'js/**',
-            //'css/*.css',
-			'css/style.css',
-            'css/flexslider.css',
-            'font/**',
-            'includes/**',
-            'index.html'
-			],
-			dest: "build"
-		}]
-	  }
+      img: {
+        expand: true,
+        // откуда
+        cwd: 'source/img/',
+        // какие файлы
+        src: ['**/*.{png,jpg,gif,svg}'],
+        // куда
+        dest: 'build/img/',
+      },
+
+      js: {
+        expand: true,
+        // откуда
+        cwd: 'source/js/',
+        // какие файлы
+        src: ['*.js'],
+        // куда
+        dest: 'build/js/',
+      },
+      
+      css: {
+        expand: true,
+        // откуда
+        cwd: 'source/css/',
+        // какие файлы
+        src: ['*.css'],
+        // куда
+        dest: 'build/css/',
+      },
+      
+      font: {
+        expand: true,
+        // откуда
+        cwd: 'source/font/',
+        // какие файлы
+        src: ['**'],
+        // куда
+        dest: 'build/font/',
+      },
+      
+      includes: {
+        expand: true,
+        // откуда
+        cwd: 'source/includes/',
+        // какие файлы
+        src: ['**'],
+        // куда
+        dest: 'build/includes/',
+      },
+      
+      html: {
+        expand: true,
+        // откуда
+        cwd: 'source/',
+        // какие файлы
+        src: ['*.html'],
+        // куда
+        dest: 'build/',
+      }
 	},
 	
     // обрабатываем разметку
@@ -48,12 +104,22 @@ module.exports = function(grunt) {
     // autoprefixer
     autoprefixer: {
       options: {
-	    browsers: ["last 2 version", "ie 10"]
+	    browsers: ["last 4 version", "ie 10"]
 	  },
 	  style: {
 		src: "build/css/style.css"
       }
 	},
+    
+    // объединяем медиавыражения
+    cmq: {
+      style: {
+        files: {
+          // в какой файл, из какого файла (тут это один и тот же файл)
+          'build/css/style.css': ['build/css/style.css']
+        }
+      }
+    },
 
     // Сжимаем css
     cssmin: {
@@ -75,6 +141,14 @@ module.exports = function(grunt) {
       }
     },
 	
+	// Сжимаем js
+    uglify: {
+      build: {
+          src: 'build/js/script.js',
+          dest: 'build/js/script.min.js'
+      }
+    },
+	
 	//Сжимаем картинки
     imagemin: { 
       images: {         
@@ -88,14 +162,14 @@ module.exports = function(grunt) {
       } 
     },
 
-    // Заменяем пути в файле index.html
+    // Заменяем пути в файлх *.html
     replace: {
       dist: {
         options: {
           patterns: [
             {
-              match: /\"js\/main.js/g, 
-              replacement: '"js/main.min.js'
+              match: /\"js\/script.js/g, 
+              replacement: '"js/script.min.js'
             },
             {
               match: /\"css\/style.css/g, 
@@ -106,9 +180,85 @@ module.exports = function(grunt) {
         files: [
           {
 			expand: true, 
-		    src: ['build/index.html']
+		    src: ['build/*.html']
 		  }
         ]
+      }
+    },
+	
+    // публикация на GitHub Pages (будет доступен в сети по адресу http://tamtamlg.github.io/НАЗВАНИЕ-РЕПОЗИТОРИЯ/)
+    'gh-pages': {
+      options: {
+        // какую папку считать результатом работы
+        base: 'build'
+      },
+      src: '**/*'
+    },
+    
+	//Отслеживаем изменения
+    watch: {
+      // перезагрузка
+      livereload: {
+        options: { livereload: true },
+        files: ['build/**/*'],
+      },
+      // следить за стилями
+      style: {
+        // за сохранением каких файлов следить
+        files: ['source/scss/**/*.scss'],
+        // какую задачу при этом запускать
+        tasks: ['style'],
+        options: {
+          spawn: false,
+        },
+      },
+      // следить за картинками
+      images: {
+        // за сохранением каких файлов следить
+        files: ['source/img/**/*.{png,jpg,gif,svg}'],
+        // какую задачу при этом запускать
+        tasks: ['img'],
+        options: {
+          spawn: false
+        },
+      },
+      // следить за файлами разметки
+      html: {
+        // за сохранением каких файлов следить
+        files: ['source/*.html'],
+        // какую задачу при этом запускать
+        tasks: ['html'],
+        options: {
+          spawn: false
+        },
+      },
+    },
+	
+	// локальный сервер, автообновление
+    browserSync: {
+      dev: {
+        bsFiles: {
+          // за изменением каких файлов следить для автообновления открытой в браузере страницы с локального сервера
+          src : [
+            'build/css/*.css',
+            'build/js/*.js',
+            'build/img/*.{png,jpg,gif,svg}',
+            'build/*.html',
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            // корень сервера
+            baseDir: "build/",
+          },
+          // синхронизация между браузерами и устройствами (если одна и та же страница открыта в нескольких браузерах)
+          ghostMode: {
+            clicks: true,
+            forms: true,
+            scroll: true
+          }
+        }
       }
     }
 
@@ -117,12 +267,44 @@ module.exports = function(grunt) {
   // задача по умолчанию
   grunt.registerTask('default', [
     'clean',
+    'sass',
     'copy',
     'includereplace:html',
     'autoprefixer',
+    'cmq',
     'cssmin',
+    'uglify',
     'imagemin',
-    'replace'
+    'replace',
+    'gh-pages',
+    'browserSync',
+	'watch'
+    
+  ]);
+  
+  // только компиляция стилей
+  grunt.registerTask('style', [
+    'sass',
+    'copy:css',
+    'cmq',
+    'cssmin'
+  ]);
+  
+  // только обработка картинок
+  grunt.registerTask('img', [
+    'copy:img',
+    'imagemin'
+  ]);
+  
+  // только обработка html
+  grunt.registerTask('html', [
+    'copy:html',
+    'includereplace:html'
   ]);
 
+  // только обработка js
+  grunt.registerTask('js', [
+    'copy:js',
+    'uglify'
+  ]);
 };
--- source/js/main.js
+++ source/js/main.js
@@ -10,14 +10,16 @@ $(function () {
   
   // меню
   var menu = $('.main-menu');
-  $('.bars').click(function (e) {
+  $('.cmn-toggle-switch').click(function (e) {
     e.preventDefault();
     if (menu.css('right') === '-300px') {
       menu.animate({right: 0}, 500);
       $('.open-menu').css('display', 'block');
+      $('.cmn-toggle-switch').addClass('active');
     } else {
       menu.animate({right: '-300px'}, 500);
       $('.open-menu').css('display', 'none');
+      $('.cmn-toggle-switch').removeClass('active');
     }
   });
   
@@ -26,11 +28,13 @@ $(function () {
     if (menu.css('right') === '0px' && !menu.is(e.target) && menu.has(e.target).length === 0) {
       menu.animate({right: '-300px'}, 500);
       $('.open-menu').css('display', 'none');
+      $('.cmn-toggle-switch').removeClass('active');
     }
   });
   
   //список городов
   var cityList = $('.city-list');
+  var cityListItem = $('.city-list li a');
   var location = $('.location');
   var city = $('.city');
   location.click(function (e) {
@@ -40,8 +44,17 @@ $(function () {
       city.css('color', 'transparent');
     } else {
       cityList.hide();
+      e.stopImmediatePropagation();
       city.css('color', '#fff');
     }
+  });
+  
+  //выбор города
+  cityListItem.click(function (e) {
+    e.preventDefault();
+    city.text($(this).text());
+    cityList.hide();
+    city.css('color', '#fff');
   });
   
   //скрываем список городов, если клик не на нем
@@ -75,10 +88,42 @@ $(function () {
     });
   });
   
+  // подключение слайдера расписания
+  $('.shedule-slider').flexslider({
+    animation: 'slide',
+    controlNav: false,
+    directionNav: true,
+    slideshow: false,
+    touch: false
+  });
   
+  //сортировка
+  var sortList = $('.sort-list');
+  var sortListItem = $('.sort-list li a');
+  var sortWrapper = $('.sort-wrapper');
+  sortWrapper.click(function (e) {
+    e.preventDefault();
+    if (sortList.css('display') === 'none') {
+      sortList.slideDown();
+    } else {
+      sortList.hide();
+    }
+  });
   
+  //выбор сортировки
+  sortListItem.click(function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    $('.sort-text').text($(this).text());
+    sortList.hide();
+  });
   
-  
+  //скрываем сортировку, если клик не на ней
+  $(document).mouseup(function (e) {
+    if (!sortWrapper.is(e.target) && sortWrapper.has(e.target).length === 0) {
+      sortList.hide();
+    }
+  });
   
 });
 
